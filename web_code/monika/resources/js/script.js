ebb9de364f0709c9a5c85fe0e7c65dc276b6254a
Eric Schrijver <eric@ericschrijver.nl>
2015-06-08 15:09:34 +0200

    Some comments to understand

--- web_code/monika/resources/js/script.js
+++ web_code/monika/resources/js/script.js
@@ -19,12 +19,16 @@ $(document).ready(function(){
 	
 
 	$(canvas).mousedown(function (e) {
+		// when you click anywhere,
+		// store mouse coordinates
 		var x = e.offsetX;
 		var y = e.offsetY;
 
+		// go from mouse coordinates to grid position
 		x = (Math.round(x / 30) * 30) - 10;
 		y = (Math.round(y / 30) * 30) - 10;
 
+		// If first Line, we do a MoveTo command.
 		if (first) {
 			paper.rect(x - 2, y - 2, 4, 4).attr({
 				stroke: 'none',
@@ -33,6 +37,7 @@ $(document).ready(function(){
 			lines[idx].attr('path', 'M' + (x ) + ',' + (y));
 			first = !first;
 		} else {
+			// Otherwise, we do a LineTo command
 			var p = lines[idx].attr('path');
 			p += 'L' + x + ',' + y;
 			lines[idx].attr('path', p);
@@ -48,6 +53,7 @@ $(document).ready(function(){
 });
 
 $(document).on({
+	// When you press space, stop this line
 	keypress: function(e){
 		if(e.which === 32){
 			lines[idx] = paper.path().attr({
@@ -62,6 +68,7 @@ $(document).on({
 
 $(document).on({
 	dblclick: function(e){
+		// When you double-click, stop this line
 		lines[idx] = paper.path().attr({
 			'stroke-width': 5,
 			'stroke-linecap': 'square'
@@ -74,11 +81,17 @@ $(document).on({
 
 
 function buildGrid(){
+	// Makes the open circles in the background
+	
+	// The width, height is based on the HTML document
 	var dW = $(document).width();
 	var dH = $(document).height();
 
+	// On the x-axis: start at 20, stop at width-10, every 30px:
 	for(var x = 20; x < dW - 10; x += 30){
+		// On the y-axis: start at 20, stop at height-10, every 30px:
 		for(var y = 20; y < dH - 10; y += 30){
+			// Make a 10 by 10px circle with a gray out-line
 			paper.ellipse(x, y, 10, 10).attr({
 				stroke: '#cdcdcd'
 			});
