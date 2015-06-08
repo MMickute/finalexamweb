d0410e3fbad3dc297e48a36cb3bd223d9cd8d96a
Eric Schrijver <eric@ericschrijver.nl>
2015-06-08 15:18:12 +0200

    Put JavaScript at the end
    And remove the Document.ready()

--- web_code/monika/resources/js/script.js
+++ web_code/monika/resources/js/script.js
@@ -3,53 +3,51 @@ var first = true;
 var lines = [];
 var idx = 0;
 
-$(document).ready(function(){
-	var $canvasContainer = $('#line-canvas');
-	canvas = document.getElementById('canvas');
+var $canvasContainer = $('#line-canvas');
+canvas = document.getElementById('canvas');
 
-	paper = new Raphael(canvas, $canvasContainer.width(), $canvasContainer.height());
-	path = paper.path();
-	buildGrid();
-	
-	lines[idx] = paper.path().attr({
-		'stroke-width': 5,
-		'stroke-linecap': 'square'
-	});
+paper = new Raphael(canvas, $canvasContainer.width(), $canvasContainer.height());
+path = paper.path();
+buildGrid();
 
-	
+lines[idx] = paper.path().attr({
+	'stroke-width': 5,
+	'stroke-linecap': 'square'
+});
 
-	$(canvas).mousedown(function (e) {
-		// when you click anywhere,
-		// store mouse coordinates
-		var x = e.offsetX;
-		var y = e.offsetY;
 
-		// go from mouse coordinates to grid position
-		x = (Math.round(x / 30) * 30) - 10;
-		y = (Math.round(y / 30) * 30) - 10;
 
-		// If first Line, we do a MoveTo command.
-		if (first) {
-			paper.rect(x - 2, y - 2, 4, 4).attr({
-				stroke: 'none',
-				fill: '#000'
-			});
-			lines[idx].attr('path', 'M' + (x ) + ',' + (y));
-			first = !first;
-		} else {
-			// Otherwise, we do a LineTo command
-			var p = lines[idx].attr('path');
-			p += 'L' + x + ',' + y;
-			lines[idx].attr('path', p);
-			idx++;
-			lines[idx] = paper.path('M' + (x ) + ',' + (y)).attr({
-				'stroke-width': 5,
-				'stroke-linecap': 'square'
-			});
-		}
-		lastY = y;
-		lastX = x;
-	});
+$(canvas).mousedown(function (e) {
+	// when you click anywhere,
+	// store mouse coordinates
+	var x = e.offsetX;
+	var y = e.offsetY;
+
+	// go from mouse coordinates to grid position
+	x = (Math.round(x / 30) * 30) - 10;
+	y = (Math.round(y / 30) * 30) - 10;
+
+	// If first Line, we do a MoveTo command.
+	if (first) {
+		paper.rect(x - 2, y - 2, 4, 4).attr({
+			stroke: 'none',
+			fill: '#000'
+		});
+		lines[idx].attr('path', 'M' + (x ) + ',' + (y));
+		first = !first;
+	} else {
+		// Otherwise, we do a LineTo command
+		var p = lines[idx].attr('path');
+		p += 'L' + x + ',' + y;
+		lines[idx].attr('path', p);
+		idx++;
+		lines[idx] = paper.path('M' + (x ) + ',' + (y)).attr({
+			'stroke-width': 5,
+			'stroke-linecap': 'square'
+		});
+	}
+	lastY = y;
+	lastX = x;
 });
 
 $(document).on({
